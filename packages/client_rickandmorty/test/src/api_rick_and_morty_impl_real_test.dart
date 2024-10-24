9f958908e26b7730505bdde681d62969ed78caec
NikitaTaverz <nikita3kovalev3@gmail.com>
2024-10-24 22:23:10 +0300

    test add

--- packages/client_rickandmorty/test/src/api_rick_and_morty_impl_real_test.dart
+++ packages/client_rickandmorty/test/src/api_rick_and_morty_impl_real_test.dart
@@ -10,6 +10,7 @@ void main() {
     apiClient = MainCoreClient().init();
   });
 
+  
   group('Api client - character', () {
     test('fetch characters returns a list of characters', () async {
       final result = await apiClient.fetchCharacters();
@@ -18,6 +19,18 @@ void main() {
       expect(result.length, 20);
       expect(result[0].name, 'Rick Sanchez');
       expect(result[1].name, 'Morty Smith');
+    });
+
+    test('fetch character by ID returns a single character', () async {
+      const testCharacterId = 1;
+      final result = await apiClient.fetchCharacterID(testCharacterId);
+
+      expect(result, isA<CharacterInfoModel>());
+      expect(result.name, 'Rick Sanchez');
+      expect(result.status, 'Alive');
+      expect(result.species, 'Human');
+      expect(result.origin.name, 'Earth (C-137)');
+      expect(result.location.name, 'Citadel of Ricks');
     });
   });
 }
--- packages/core_app/test/src/character/domain/character_repository_interface_test.mocks.dart
+++ packages/core_app/test/src/character/domain/character_repository_interface_test.mocks.dart
@@ -3,12 +3,12 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i3;
+import 'dart:async' as _i4;
 
 import 'package:core_app/src/character/domain/character_repository_interface.dart'
-    as _i2;
+    as _i3;
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:models_app/models_app.dart' as _i4;
+import 'package:models_app/models_app.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -23,26 +23,54 @@ import 'package:models_app/models_app.dart' as _i4;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
+class _FakeCharacterInfoModel_0 extends _i1.SmartFake
+    implements _i2.CharacterInfoModel {
+  _FakeCharacterInfoModel_0(
+    Object parent,
+    Invocation parentInvocation,
+  ) : super(
+          parent,
+          parentInvocation,
+        );
+}
+
 /// A class which mocks [ICharacterRepository].
 ///
 /// See the documentation for Mockito's code generation for more information.
 class MockICharacterRepository extends _i1.Mock
-    implements _i2.ICharacterRepository {
+    implements _i3.ICharacterRepository {
   MockICharacterRepository() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i3.Future<List<_i4.Character>> getCharacters() => (super.noSuchMethod(
+  _i4.Future<List<_i2.Character>> getCharacters() => (super.noSuchMethod(
         Invocation.method(
           #getCharacters,
           [],
         ),
-        returnValue: _i3.Future<List<_i4.Character>>.value(<_i4.Character>[]),
-      ) as _i3.Future<List<_i4.Character>>);
+        returnValue: _i4.Future<List<_i2.Character>>.value(<_i2.Character>[]),
+      ) as _i4.Future<List<_i2.Character>>);
 
   @override
-  _i3.Future<void> toggleLike({
+  _i4.Future<_i2.CharacterInfoModel> fetchCharacterID(int? characterId) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #fetchCharacterID,
+          [characterId],
+        ),
+        returnValue:
+            _i4.Future<_i2.CharacterInfoModel>.value(_FakeCharacterInfoModel_0(
+          this,
+          Invocation.method(
+            #fetchCharacterID,
+            [characterId],
+          ),
+        )),
+      ) as _i4.Future<_i2.CharacterInfoModel>);
+
+  @override
+  _i4.Future<void> toggleLike({
     required int? idCharacter,
     required bool? liked,
   }) =>
@@ -55,16 +83,16 @@ class MockICharacterRepository extends _i1.Mock
             #liked: liked,
           },
         ),
-        returnValue: _i3.Future<void>.value(),
-        returnValueForMissingStub: _i3.Future<void>.value(),
-      ) as _i3.Future<void>);
+        returnValue: _i4.Future<void>.value(),
+        returnValueForMissingStub: _i4.Future<void>.value(),
+      ) as _i4.Future<void>);
 
   @override
-  _i3.Future<List<_i4.Character>> getLikedCharacters() => (super.noSuchMethod(
+  _i4.Future<List<_i2.Character>> getLikedCharacters() => (super.noSuchMethod(
         Invocation.method(
           #getLikedCharacters,
           [],
         ),
-        returnValue: _i3.Future<List<_i4.Character>>.value(<_i4.Character>[]),
-      ) as _i3.Future<List<_i4.Character>>);
+        returnValue: _i4.Future<List<_i2.Character>>.value(<_i2.Character>[]),
+      ) as _i4.Future<List<_i2.Character>>);
 }
